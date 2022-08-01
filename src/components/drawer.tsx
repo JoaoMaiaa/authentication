620b62777207791e0c2faf18640a8d181f1627d6
João Maia <15joaomaia@gmail.com>
2022-08-01 11:52:55 -0300

    tirando a imagen do drawer

--- src/components/drawer.tsx
+++ src/components/drawer.tsx
@@ -8,15 +8,20 @@ import {
   DrawerCloseButton,
   Button,
   useDisclosure,
-  Img,
   Box,
   Text
 } from '@chakra-ui/react'
+import { useRouter } from 'next/router'
 import React, { useRef } from 'react'
 
 const DrawerComponent = () => {
   const { isOpen, onOpen, onClose } = useDisclosure()
   const btnRef = useRef(null)
+  const router = useRouter()
+
+  function handleDelete() {
+    router.push('/')
+  }
 
   return (
     <>
@@ -38,45 +43,7 @@ const DrawerComponent = () => {
           <DrawerCloseButton />
           <DrawerHeader>Seus dados</DrawerHeader>
           <DrawerBody>
-            <Box>
-              <Img
-                mx="auto"
-                w="8rem"
-                h="8rem"
-                objectFit="cover"
-                borderRadius="3"
-                src="/your-account.jpg"
-              />
-
-              {/* <Box display="flex" justifyContent="center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="50"
-                  height="50"
-                  fill="currentColor"
-                  className="bi bi-person-fill"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-                </svg>
-              </Box> */}
-              <Box mt="1rem" display="flex" justifyContent="around">
-                <Text my="auto">Adcione uma foto</Text>
-                <Button ml="auto">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    className="bi bi-pencil"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
-                  </svg>
-                </Button>
-              </Box>
-            </Box>
-            <Box mt="3rem">
+            <Box mt="2rem">
               <Text>
                 Nome: <Box fontWeight="light">João Maia</Box>
               </Text>
@@ -86,8 +53,15 @@ const DrawerComponent = () => {
             </Box>
           </DrawerBody>
           <DrawerFooter>
-            <Button mr="auto">Sair da conta</Button>
-            <Button color="white" bg="red.500" _hover={{ bg: 'red.600' }}>
+            <Button onClick={() => router.push('/')} mr="auto">
+              Sair da conta
+            </Button>
+            <Button
+              onClick={() => handleDelete}
+              color="white"
+              bg="red.500"
+              _hover={{ bg: 'red.600' }}
+            >
               Excluir conta
             </Button>
           </DrawerFooter>
